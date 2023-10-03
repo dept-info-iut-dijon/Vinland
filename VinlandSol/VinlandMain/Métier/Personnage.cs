27ab04633ea74e0b2241bd38b87147e961d2865a
AlexisParis2 <alexis.paris@iut-dijon.u-bourgogne.fr>
2023-10-03 11:07:00 +0200

    Squelettes classes Carte et Personnage

---
+++ VinlandSol/VinlandMain/Métier/Personnage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinlandSol.Métier
+{
+    public class Personnage
+    {
+        private string nom;
+        private DateTime datecreation;
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public DateTime Datecreation
+        {
+            get { return datecreation; }
+            set { datecreation = value; }
+        }
+
+        public Personnage()
+        {
+
+        }
+
+        public static void AjouterPersonnage(List<Personnage> listePersonnages, Personnage nouveauPersonnage)
+        {
+            listePersonnages.Add(nouveauPersonnage);
+        }
+
+        public static void SupprimerPersonnage(List<Personnage> listePersonnages, Personnage personnageASupprimer)
+        {
+            listePersonnages.Remove(personnageASupprimer);
+        }
+    }
+}
