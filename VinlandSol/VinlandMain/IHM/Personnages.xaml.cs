64d490bd17b564b5376769005e09c4009208e5b3
Aaron-Gritti <aaron.gritti@iut-dijon.u-bourgogne.fr>
2023-10-04 02:53:35 +0200

    Non-fermeture de l'application Fixed avec Personnages et AjouterPersonnage

--- VinlandSol/VinlandMain/IHM/Personnages.xaml.cs
+++ VinlandSol/VinlandMain/IHM/Personnages.xaml.cs
@@ -20,21 +20,42 @@ namespace VinlandSol.IHM
     /// </summary>
     public partial class Personnages : Window
     {
-        private AjouterPersonnage pagecreationperso;
+        private AjouterPersonnage? pageajouterPerso;
+        private bool ajouterPersoOpen = false;
 
         public Personnages()
         {
             InitializeComponent();
-            pagecreationperso = new AjouterPersonnage();
+            Closed += ShutdownEnForce; // ShutdownEnForce est appelé à la fermeture de cette fenêtre
         }
 
+        /// <summary>
+        /// Ouvre la fenêtre Campagnes et ferme la fenêtre actuelle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OuvrirCampagnes_Click(object sender, RoutedEventArgs e)
         {
             Campagnes pagecreation = new Campagnes();
             pagecreation.Show();
-            PersonnagesWindow.Close();
+
+            var timer = new System.Timers.Timer(100); // Délai de 100 millisecondes 
+            timer.Elapsed += (s, args) =>
+            {
+                timer.Stop(); 
+                Dispatcher.Invoke(() =>
+                {
+                    PersonnagesWindow.Close(); // On ferme cette fenêtre en retard pour éviter que le if de ShutdownEnForce ne passe.
+                });
+            };
+            timer.Start();
         }
 
+        /// <summary>
+        /// Ouvre la fenêtre Cartes et ferme la fenêtre actuelle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OuvrirCartes_Click(object sender, RoutedEventArgs e)
         {
             Cartes pagecreation = new Cartes();
@@ -42,23 +63,49 @@ namespace VinlandSol.IHM
             PersonnagesWindow.Close();
         }
 
+        /// <summary>
+        /// Ouvre la fenêtre AjouterPersonnage et limite cette action tant que cette dernière n'est pas fermée
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OuvrirAjouterPersonnage_Click(object sender, RoutedEventArgs e)
         {
-            if (pagecreationperso.IsVisible == false)
+            if (ajouterPersoOpen == false)
             {
-                pagecreationperso = new AjouterPersonnage();
-                pagecreationperso.Show();
+                pageajouterPerso = new AjouterPersonnage();
+                pageajouterPerso.Closed += AjouterPerso_Closed;
+                pageajouterPerso.Show();
+                ajouterPersoOpen = true;
             }
         }
 
-        private void EditionNom(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Force le shutdown de l'application quand AjouterPersonnage est la dernière fenêtre à être fermée
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AjouterPerso_Closed(object sender, EventArgs e)
         {
-            
+            ajouterPersoOpen = false;
+            if (Application.Current.Windows.Count == 1)
+            {
+                Application.Current.Shutdown();
+            }
+
         }
 
-        private void ValiderNom(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Force le shutdown de l'application quand cette fenêtre est la dernière a être fermée
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShutdownEnForce(object sender, EventArgs e)
         {
-
+            if (Application.Current.Windows.Count == 1)
+            {
+                
+                Application.Current.Shutdown();
+            }
         }
     }
 }
