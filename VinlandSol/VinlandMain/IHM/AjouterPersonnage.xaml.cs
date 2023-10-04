a7872aa1991a8239d78b249d0ecf23a6864eac0e
AlexisParis2 <alexis.paris@iut-dijon.u-bourgogne.fr>
2023-10-04 11:18:32 +0200

    Implémentation méthode de sauvegarde dans un .txt dans ajouter un personnage

--- VinlandSol/VinlandMain/IHM/AjouterPersonnage.xaml.cs
+++ VinlandSol/VinlandMain/IHM/AjouterPersonnage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,42 @@ namespace VinlandSol.IHM
         public AjouterPersonnage()
         {
             InitializeComponent();
+
+            btnAjouter.Click += BtnAjouter_Click;
+        }
+
+        private void BtnAjouter_Click(object sender, RoutedEventArgs e)
+        {
+            // Récupérez le contenu des TextBox
+            string idJoueur = IdJoueur.Text;
+            string idPersonnage = IdPersonnage.Text;
+
+            // Vérifiez que les données ne sont pas vides
+            if (!string.IsNullOrEmpty(idJoueur) && !string.IsNullOrEmpty(idPersonnage))
+            {
+                // Créez ou ouvrez un fichier .txt pour sauvegarder les données
+                string filePath = "personnages.txt";
+
+                using (StreamWriter sw = File.AppendText(filePath))
+                {
+                    // Écrivez les données dans le fichier
+                    sw.WriteLine($"ID du joueur: {idJoueur}, Nom du personnage: {idPersonnage}");
+                }
+
+                // Effacez les TextBox après sauvegarde
+                IdJoueur.Clear();
+                IdPersonnage.Clear();
+
+                // Affichez un message de confirmation
+                MessageBox.Show("Personnage ajouté avec succès !");
+
+                
+            }
+            else
+            {
+                // Affichez un message d'erreur si les données sont vides
+                MessageBox.Show("Veuillez remplir toutes les informations avant de créer un personnage");
+            }
         }
     }
 }
