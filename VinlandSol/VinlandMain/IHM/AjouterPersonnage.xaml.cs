ae410f0732e37a5ae66faec2ee37b5a380820ab6
AlexisParis2 <alexis.paris@iut-dijon.u-bourgogne.fr>
2023-10-06 18:14:06 +0200

    Clean des commentaires AjouterPersonnage

--- VinlandSol/VinlandMain/IHM/AjouterPersonnage.xaml.cs
+++ VinlandSol/VinlandMain/IHM/AjouterPersonnage.xaml.cs
@@ -32,33 +32,29 @@ namespace VinlandSol.IHM
         /// <param name="e"></param>
         private void BtnAjouter_Click(object sender, RoutedEventArgs e)
         {
-            // Récupérez le contenu des TextBox
+            // Récupération du contenu des TextBox
             string idJoueur = IdJoueur.Text;
             string idPersonnage = IdPersonnage.Text;
             DateTime dateCreation = DateTime.Now;
 
 
-            // Vérifiez que les données ne sont pas vides
+            
             if (!string.IsNullOrEmpty(idJoueur) && !string.IsNullOrEmpty(idPersonnage))
             {
-                // Créez ou ouvrez un fichier .txt pour sauvegarder les données
-                string filePath = "personnages.txt";
+                
+                string filePath = "personnages.txt"; // Cela créé un fichier si il n'y en a pas
 
                 using (StreamWriter sw = File.AppendText(filePath))
                 {
-                    // Écrivez les données dans le fichier
                     sw.WriteLine($"{idJoueur}, {idPersonnage}");
                 }
-                // Effacez les TextBox après sauvegarde
                 IdJoueur.Clear();
                 IdPersonnage.Clear();
 
-                // Affichez un message de confirmation
                 MessageBox.Show("Personnage ajouté avec succès !");                
             }
             else
             {
-                // Affichez un message d'erreur si les données sont vides
                 MessageBox.Show("Veuillez remplir toutes les informations avant de créer un personnage");
             }
         }
