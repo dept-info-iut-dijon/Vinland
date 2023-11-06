a53e38bc1d8771ecc1422ceaf8d30721c28a1923
Aaron-Gritti <aaron.gritti@iut-dijon.u-bourgogne.fr>
2023-11-06 16:20:44 +0100

    Déplacement de la branche BDD à Métier

---
+++ VinlandSol/VinlandMain/Métier/Personnage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinlandSol.Métier
+{
+    public class Personnage
+    {
+        public int ID { get; set; }
+        public string Nom { get; set; }
+        public int ID_Joueur { get; set; }
+        public int ID_Campagne { get; set; }
+
+        public Personnage(int id, string nom, int id_joueur, int id_campagne)
+        {
+            this.ID = id;
+            this.Nom = nom;
+            this.ID_Joueur = id_joueur;
+            this.ID_Campagne = id_campagne;            
+        }
+
+    }
+
+}
---
+++ VinlandSol/VinlandMain/Métier/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinlandSol.Métier
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Mdp { get; set; }
+        public Joueur? Joueur { get; set; }
+        public MJ? MJ { get; set; }
+
+        public User(int id, string nom, string mdp) 
+        {
+            Id = id;
+            Nom = nom;
+            Mdp = mdp;
+            Joueur = null; // Joueur = JoueurDAO.GetJoueurById(joueurId); 
+            MJ = null; // MJ = MJDAO.GetMJById(mjId); 
+        }
+    }
+
+
+    public class Joueur
+    {
+        public int ID { get; set; }
+    }
+
+
+    public class MJ
+    {
+        public int ID { get; set; }
+    }
+}
