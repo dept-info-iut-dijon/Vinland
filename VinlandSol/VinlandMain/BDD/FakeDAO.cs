9eed522da55fd96d33fe63fee8c977bc74fd6c78
AlexisParis2 <alexis.paris@iut-dijon.u-bourgogne.fr>
2023-11-09 16:32:08 +0100

    Implémentation des fonctions nécessaires au FakeDAO

--- VinlandSol/VinlandMain/BDD/FakeDAO.cs
+++ VinlandSol/VinlandMain/BDD/FakeDAO.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VinlandSol.IHM;
 using VinlandSol.Métier;
 using Carte = VinlandSol.Métier.Carte;
 
@@ -28,16 +23,9 @@ namespace VinlandSol.BDD
             cartes = new List<Carte>();
         }
 
-        public void CreateJoueur(string nom, string mdp)
+        public void AjouterCampagne(Campagne campagne)
         {
-            int id =  joueurs.Count+1;
-            Joueur newUser = new Joueur(id, nom, mdp);
-            joueurs.Add(newUser);
-        }
-
-        public bool VerifyUserAccount(string username, string password)
-        {
-            return (joueurs.Any(account => account.Nom == username && account.Mdp == password) || mjs.Any(account => account.Nom == username && account.Mdp == password));
+            campagnes.Add(campagne);
         }
 
         public void AjouterJoueur(Joueur joueur)
@@ -45,31 +33,9 @@ namespace VinlandSol.BDD
             joueurs.Add(joueur);
         }
 
-        public List<Joueur> GetJoueurs()
+        public void AjouterMJ(MJ mj)
         {
-            return joueurs;
-        }
-
-        public void AjouterMJ(string nom, string mdp)
-        {
-            int id = mjs.Count + 1;
-            MJ mj = new MJ(id, nom, mdp);
             mjs.Add(mj);
-        }
-
-        public List<MJ> GetMJs()
-        {
-            return mjs;
-        }
-
-        public void AjouterCampagne(Campagne campagne)
-        {
-            campagnes.Add(campagne);
-        }
-
-        public List<Campagne> GetCampagnes()
-        {
-            return campagnes;
         }
 
         public void AjouterPersonnage(Personnage personnage)
@@ -77,9 +43,185 @@ namespace VinlandSol.BDD
             personnages.Add(personnage);
         }
 
+        public void AjouterCarte(Carte carte)
+        {
+            cartes.Add(carte);
+        }
+
+        public void CreateJoueur(string nom, string mdp)
+        {
+            int id = joueurs.Count + 1;
+            Joueur newUser = new Joueur(id, nom, mdp);
+            joueurs.Add(newUser);
+        }
+
+        public void CreateMJ(string nom, string mdp)
+        {
+            int id = mjs.Count + 1;
+            MJ newMJ = new MJ(id, nom, mdp);
+            mjs.Add(newMJ);
+        }
+
+        public void CreatePersonnage(string nom, Joueur joueur, Campagne campagne)
+        {
+            int id = personnages.Count + 1;
+            Personnage newCharacter = new Personnage(id, nom, joueur, campagne);
+            personnages.Add(newCharacter);
+        }
+
+        public void CreateCarte(int hauteur, int largeur, Campagne campagne)
+        {
+            int id = cartes.Count + 1;
+            Carte newMap = new Carte(id, hauteur, largeur, campagne);
+            cartes.Add(newMap);
+        }
+        public void CreateCampagne(string nom)
+        {
+            int id = campagnes.Count + 1;
+            Campagne newCampagne = new Campagne(id, nom);
+            campagnes.Add(newCampagne);
+        }
+
+        public void DeletePersonnage(int id)
+        {
+            if (personnages.Count != 0)
+            {
+                for (int i = 0; i < personnages.Count; i++)
+                {
+                    if (personnages[i].Id == id) personnages.RemoveAt(i);
+                }
+            }
+        }
+
+        public void DeleteCarte(int id)
+        {
+            if (cartes.Count != 0)
+            {
+                for (int i = 0; i < cartes.Count; i++)
+                {
+                    if (cartes[i].Id == id) cartes.RemoveAt(i);
+                }
+            }
+        }
+
+        public void DeleteCampagne(int id)
+        {
+            if (campagnes.Count != 0)
+            {
+                for (int i = 0; i < campagnes.Count; i++)
+                {
+                    if (campagnes[i].ID == id) campagnes.RemoveAt(i);
+                }
+            }
+        }
+
+        public List<Campagne> GetCampagnes()
+        {
+            return campagnes;
+        }
+
+        /// <summary>
+        /// Donne la campagne demandée
+        /// </summary>
+        /// <param name="id">l'id de la campagne demandée</param>
+        /// <returns>Une campagne ou null</returns>
+        public Campagne GetCampagne(int id)
+        {
+            if (campagnes.Count == 0)
+            {
+                return null;
+            }
+
+            Campagne campagne = null;
+            for (int i = 0; i < campagnes.Count; i++)
+            {
+                if (campagnes[i].ID == id) campagne = campagnes[i];
+            }
+            return campagne;
+        }
+
+        public Personnage GetPersonnage(int id)
+        {
+            if (personnages.Count == 0)
+            {
+                return null;
+            }
+
+            Personnage character = null;
+            for (int i = 0; i < personnages.Count; i++)
+            {
+                if (personnages[i].Id == id) character = personnages[i];
+            }
+            return character;
+        }
+
+        public Carte GetCarte(int id)
+        {
+            if (cartes.Count == 0)
+            {
+                return null;
+            }
+
+            Carte map = null;
+            for (int i = 0; i < cartes.Count; i++)
+            {
+                if (cartes[i].Id == id) map = cartes[i];
+            }
+            return map;
+        }
+
+        public List<Joueur> GetJoueurs()
+        {
+            return joueurs;
+        }
+
+        public List<MJ> GetMJs()
+        {
+            return mjs;
+        }
+
         public List<Personnage> GetPersonnages()
         {
             return personnages;
         }
+
+        public void UpdateCampagne(int id, Campagne campagne)
+        {
+            if (campagnes.Count != 0)
+            {
+                for (int i = 0; i < campagnes.Count; i++)
+                {
+                    if (campagnes[i].ID == campagne.ID) campagnes[i] = campagne;
+                }
+            }
+        }
+
+        public void UpdatePersonnage(int id, Personnage personnage)
+        {
+            if (personnages.Count != 0)
+            {
+                for (int i = 0; i < personnages.Count; i++)
+                {
+                    if (personnages[i].Id == personnage.Id) personnages[i] = personnage;
+                }
+            }
+        }
+
+        public void UpdateCarte(int id, Carte carte)
+        {
+            if (cartes.Count != 0)
+            {
+                for (int i = 0; i < cartes.Count; i++)
+                {
+                    if (cartes[i].Id == carte.Id) cartes[i] = carte;
+                }
+            }
+        }
+
+        public bool VerifyUserAccount(string username, string password)
+        {
+            return (joueurs.Any(account => account.Nom == username && account.Mdp == password) || mjs.Any(account => account.Nom == username && account.Mdp == password));
+        }
+
     }
 }
