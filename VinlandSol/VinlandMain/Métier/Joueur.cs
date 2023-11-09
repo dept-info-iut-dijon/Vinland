c437df807db0f354e417a5fb8c52d0039dfaf77d
AlexisParis2 <alexis.paris@iut-dijon.u-bourgogne.fr>
2023-11-09 15:26:41 +0100

    Rework des classes Métier

--- VinlandSol/VinlandMain/Métier/Joueur.cs
+++ VinlandSol/VinlandMain/Métier/Joueur.cs
@@ -1,16 +1,23 @@
-﻿namespace VinlandSol.Métier
+﻿using System.Collections.Generic;
+
+namespace VinlandSol.Métier
 {
+    /// <summary>
+    /// Un joueur
+    /// </summary>
     public class Joueur : IUser
     {
-        public int Id { get ; set; }
+        public int Id { get; set; }
         public string Nom { get; set; }
         public string Mdp { get; set; }
-    
-        public Joueur(int id, string nom, string mdp) 
-        { 
+        public List<Personnage> Personnages { get; set; } // La liste des Personnages de ce Joueur
+
+        public Joueur(int id, string nom, string mdp)
+        {
             Id = id;
             Nom = nom;
             Mdp = mdp;
+            Personnages = new List<Personnage>();
         }
 
     }
--- VinlandSol/VinlandMain/Métier/Personnage.cs
+++ VinlandSol/VinlandMain/Métier/Personnage.cs
@@ -6,19 +6,22 @@ using System.Threading.Tasks;
 
 namespace VinlandSol.Métier
 {
+    /// <summary>
+    /// Un personnage
+    /// </summary>
     public class Personnage
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Nom { get; set; }
-        public int ID_Joueur { get; set; }
-        public int ID_Campagne { get; set; }
+        public Joueur Joueur { get; set; } // Le Joueur propriétaire de ce Personnage
+        public Campagne Campagne { get; set; } // La Campagne dont le Personnage fait partie
 
-        public Personnage(int id, string nom, int id_joueur, int id_campagne)
+        public Personnage(int id, string nom, Joueur joueur, Campagne campagne)
         {
-            this.ID = id;
+            this.Id = id;
             this.Nom = nom;
-            this.ID_Joueur = id_joueur;
-            this.ID_Campagne = id_campagne;            
+            this.Joueur = joueur;
+            this.Campagne = campagne;
         }
 
     }
