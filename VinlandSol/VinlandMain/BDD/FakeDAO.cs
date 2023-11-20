4fff39f8c3f5ed6b7e974aae84dd368879979b79
AlexisParis2 <alexis.paris@iut-dijon.u-bourgogne.fr>
2023-11-20 15:12:22 +0100

    FakeDAO : Ajout de l'ensemble des commentaires

--- VinlandSol/VinlandMain/BDD/FakeDAO.cs
+++ VinlandSol/VinlandMain/BDD/FakeDAO.cs
@@ -6,6 +6,9 @@ using Carte = VinlandSol.Métier.Carte;
 
 namespace VinlandSol.BDD
 {
+    /// <summary>
+    /// Classe du DAO
+    /// </summary>
     class FakeDAO
     {
         private List<Joueur> joueurs;
@@ -14,7 +17,9 @@ namespace VinlandSol.BDD
         private List<Personnage> personnages;
         private List<Carte> cartes;
 
-
+        /// <summary>
+        /// Constructeur de la classe 
+        /// </summary>
         public FakeDAO()
         {
             joueurs = new List<Joueur>();
@@ -24,31 +29,56 @@ namespace VinlandSol.BDD
             cartes = new List<Carte>();
         }
 
+        /// <summary>
+        /// Permet l'ajout d'une campagne
+        /// </summary>
+        /// <param name="campagne">Une simple campagne</param>
         public void AjouterCampagne(Campagne campagne)
         {
             campagnes.Add(campagne);
         }
 
+        /// <summary>
+        /// Permet l'ajout d'un joueur
+        /// </summary>
+        /// <param name="joueur">Un simple joueur</param>
         public void AjouterJoueur(Joueur joueur)
         {
             joueurs.Add(joueur);
         }
 
+        /// <summary>
+        /// Permet l'ajout d'un maître de jeu
+        /// </summary>
+        /// <param name="mj"></param>
         public void AjouterMJ(MJ mj)
         {
             mjs.Add(mj);
         }
 
+        /// <summary>
+        /// Permet l'ajout de personnages
+        /// </summary>
+        /// <param name="personnage">Un simple personnage</param>
         public void AjouterPersonnage(Personnage personnage)
         {
             personnages.Add(personnage);
         }
 
+        /// <summary>
+        /// Permet l'ajout d'une carte 
+        /// </summary>
+        /// <param name="carte">Une carte</param>
         public void AjouterCarte(Carte carte)
         {
             cartes.Add(carte);
         }
 
+        /// <summary>
+        /// Ajoute un joueur à la liste des joueurs, permettant ainsi sa création
+        /// </summary>
+        /// <param name="nom">Nom d'un joueur</param>
+        /// <param name="mdp">Mot de passe d'un joueur</param>
         public void CreateJoueur(string nom, string mdp)
         {
             int id = joueurs.Count + 1;
@@ -56,6 +86,11 @@ namespace VinlandSol.BDD
             joueurs.Add(newUser);
         }
 
+        /// <summary>
+        /// Ajoute un MJ à la liste des MJ's, permettant ainsi sa création
+        /// </summary>
+        /// <param name="nom">Nom d'un MJ</param>
+        /// <param name="mdp">Mot de passe d'un MJ</param>
         public void CreateMJ(string nom, string mdp)
         {
             int id = mjs.Count + 1;
@@ -63,6 +98,12 @@ namespace VinlandSol.BDD
             mjs.Add(newMJ);
         }
 
+        /// <summary>
+        /// Ajoute un personnage à la liste des personnages, permettant ainsi sa création
+        /// </summary>
+        /// <param name="nom">Nom d'un joueur</param>
+        /// <param name="mdp">Mot de passe d'un joueur</param>
+        /// <param name="campagne">Campagne souhaitée pour un personnage</param>
         public void CreatePersonnage(string nom, Joueur joueur, Campagne campagne)
         {
             int id = personnages.Count + 1;
@@ -70,12 +111,23 @@ namespace VinlandSol.BDD
             personnages.Add(newCharacter);
         }
 
+        /// <summary>
+        /// Ajoute une carte à la liste des cartes, permettant ainsi sa création
+        /// </summary>
+        /// <param name="hauteur">Hauteur d'une carte</param>
+        /// <param name="largeur">Largeur d'une carte</param>
+        /// <param name="campagne">Campagne visée</param>
         public void CreateCarte(int hauteur, int largeur, Campagne campagne)
         {
             int id = cartes.Count + 1;
             Carte newMap = new Carte(id, hauteur, largeur, campagne);
             cartes.Add(newMap);
         }
+
+        /// <summary>
+        /// Ajoute une campagne à la liste des campagnes, permettant ainsi sa création et son accès
+        /// </summary>
+        /// <param name="nom">Nom d'une campagne</param>
         public void CreateCampagne(string nom)
         {
             int id = campagnes.Count + 1;
@@ -83,6 +135,10 @@ namespace VinlandSol.BDD
             campagnes.Add(newCampagne);
         }
 
+        /// <summary>
+        /// Permet la suppression d'un personnage par son identifiant 
+        /// </summary>
+        /// <param name="id">Identifiant permettant de différencier une personnage</param>
         public void DeletePersonnage(int id)
         {
             if (personnages.Count != 0)
@@ -94,6 +150,10 @@ namespace VinlandSol.BDD
             }
         }
 
+        /// <summary>
+        /// Permet la suppression d'une carte par son identifiant 
+        /// </summary>
+        /// <param name="id">Identifiant permettant de différencier une carte</param>
         public void DeleteCarte(int id)
         {
             if (cartes.Count != 0)
@@ -105,6 +165,10 @@ namespace VinlandSol.BDD
             }
         }
 
+        /// <summary>
+        /// Permet la suppression d'une campagne par son identifiant 
+        /// </summary>
+        /// <param name="id">Identifiant permettant de différencier une campagne</param>
         public void DeleteCampagne(int id)
         {
             if (campagnes.Count != 0)
@@ -116,6 +180,10 @@ namespace VinlandSol.BDD
             }
         }
 
+        /// <summary>
+        /// Donne la liste des campagnes 
+        /// </summary>
+        /// <returns>une liste</returns>
         public List<Campagne> GetCampagnes()
         {
             return campagnes;
@@ -141,6 +209,11 @@ namespace VinlandSol.BDD
             return campagne;
         }
 
+        /// <summary>
+        /// Permet l'aquisition d'un personnage
+        /// </summary>
+        /// <param name="id">l'identifiant d'un personnage</param>
+        /// <returns>Un personnage</returns>
         public Personnage GetPersonnage(int id)
         {
             if (personnages.Count == 0)
@@ -156,6 +229,11 @@ namespace VinlandSol.BDD
             return character;
         }
 
+        /// <summary>
+        /// Permet l'acquisition d'une carte
+        /// </summary>
+        /// <param name="id">l'identifiant d'une carte</param>
+        /// <returns></returns>
         public Carte GetCarte(int id)
         {
             if (cartes.Count == 0)
@@ -171,21 +249,38 @@ namespace VinlandSol.BDD
             return map;
         }
 
+        /// <summary>
+        /// Retourne la liste des joueurs
+        /// </summary>
+        /// <returns>Une liste</returns>
         public List<Joueur> GetJoueurs()
         {
             return joueurs;
         }
 
+        /// <summary>
+        /// Retourne la liste des MJS
+        /// </summary>
+        /// <returns>Une liste</returns>
         public List<MJ> GetMJs()
         {
             return mjs;
         }
 
+        /// <summary>
+        /// Retourne la liste des personnages
+        /// </summary>
+        /// <returns>Une liste</returns>
         public List<Personnage> GetPersonnages()
         {
             return personnages;
         }
 
+        /// <summary>
+        /// Met à jour la campagne
+        /// </summary>
+        /// <param name="id">Identifiant d'une campagne</param>
+        /// <param name="campagne">Une campagne</param>
         public void UpdateCampagne(int id, Campagne campagne)
         {
             /*
@@ -209,6 +304,12 @@ namespace VinlandSol.BDD
                 }
             }
         }
+
+        /// <summary>
+        /// Met à jour une campagne en lui attribuant un nouveau nom
+        /// </summary>
+        /// <param name="id">Identifiant d'une campagne</param>
+        /// <param name="newNom">Nouveau nom</param>
         public void UpdateCampagneName(int id, string newNom)
         {
             if (campagnes.Count != 0)
@@ -224,7 +325,11 @@ namespace VinlandSol.BDD
             }
         }
 
-        // Renommez la méthode existante pour refléter sa fonction
+        /// <summary>
+        /// Mettre à jour les dates dans les campagnes
+        /// </summary>
+        /// <param name="id">Identifiant d'une campagne</param>
+        /// <param name="campagne">une campagne</param>
         public void UpdateCampagneDetails(int id, Campagne campagne)
         {
             if (campagnes.Count != 0)
@@ -240,6 +345,11 @@ namespace VinlandSol.BDD
             }
         }
 
+        /// <summary>
+        /// Mets à jour un personnage et ses informations
+        /// </summary>
+        /// <param name="id">identifiant d'un personnage</param>
+        /// <param name="personnage">un personnage</param>
         public void UpdatePersonnage(int id, Personnage personnage)
         {
             if (personnages.Count != 0)
@@ -251,6 +361,11 @@ namespace VinlandSol.BDD
             }
         }
 
+        /// <summary>
+        /// Permet la mise à jour d'une carte 
+        /// </summary>
+        /// <param name="id">Identifiant d'une carte</param>
+        /// <param name="carte">Une carte</param>
         public void UpdateCarte(int id, Carte carte)
         {
             if (cartes.Count != 0)
@@ -262,6 +377,12 @@ namespace VinlandSol.BDD
             }
         }
 
+        /// <summary>
+        /// Permet de vérifier si un compte valide est saisi
+        /// </summary>
+        /// <param name="username">Nom d'utilisateur</param>
+        /// <param name="password">Mot de passe</param>
+        /// <returns>un accès ou non</returns>
         public bool VerifyUserAccount(string username, string password)
         {
             return (joueurs.Any(account => account.Nom == username && account.Mdp == password) || mjs.Any(account => account.Nom == username && account.Mdp == password));
