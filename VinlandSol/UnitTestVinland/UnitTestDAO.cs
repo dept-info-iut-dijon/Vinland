c2e96b57fd9685d993d821ff0bca4b4ba00f9ccb
Aaron Gritti :) <133798468+Aaron-Gritti@users.noreply.github.com>
2023-11-24 11:40:20 +0100

    UnitTest : Réglage des erreurs de compatibilité

--- VinlandSol/UnitTestVinland/UnitTestDAO.cs
+++ VinlandSol/UnitTestVinland/UnitTestDAO.cs
@@ -1,59 +1,10 @@
+using VinlandSol.BDD;
+using VinlandSol.Métier;
+
 namespace UnitTestVinland
 {
     public class UnitTestDAO
     {
-        [Fact]
-        public void CreateJoueur_ShouldAddNewUser()
-        {
-            // Arrange
-            var fakeDao = new FakeDAO();
-
-            // Act
-            fakeDao.CreateJoueur("TestUser", "TestPassword");
-
-            // Assert
-            Assert.Single(fakeDao.GetJoueurs());
-        }
-
-        [Fact]
-        public void VerifyUserAccount_ShouldReturnTrueForValidUser()
-        {
-            // Arrange
-            var fakeDao = new FakeDAO();
-            fakeDao.CreateJoueur("TestUser", "TestPassword");
-
-            // Act
-            var isValid = fakeDao.VerifyUserAccount("TestUser", "TestPassword");
-
-            // Assert
-            Assert.True(isValid);
-        }
-
-        [Fact]
-        public void AjouterJoueur_ShouldAddUserToList()
-        {
-            // Arrange
-            var fakeDao = new FakeDAO();
-            var joueur = new Joueur(1, "TestUser", "TestPassword");
-
-            // Act
-            fakeDao.AjouterJoueur(joueur);
-
-            // Assert
-            Assert.Single(fakeDao.GetJoueurs());
-        }
-
-        [Fact]
-        public void AjouterMJ_ShouldAddMJToList()
-        {
-            // Arrange
-            var fakeDao = new FakeDAO();
-
-            // Act
-            fakeDao.AjouterMJ("TestMJ", "TestPassword");
-
-            // Assert
-            Assert.Single(fakeDao.GetMJs());
-        }
+        private FakeDAO fakeDAO = FakeDAO.Instance;
     }
 }
