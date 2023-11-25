afbdf48ad37577736c2938c47ccc813767ca7c1c
Aaron Gritti :) <133798468+Aaron-Gritti@users.noreply.github.com>
2023-11-26 00:33:59 +0100

    Campagnes.xaml : Modification de la logique des évènements visuels + Nettoyage complet, documentation et ajout de commentaires CreationCompte.xaml : Nettoyage Complet, documentation et ajout de commentaires CustomMessageBox.xaml : On peut maintenant créer cette page pour confirmer une action utilisateur (choix oui ou non) + Nettoyage Complet, documentation et ajout de commentaires Vinland.xaml : Nettoyage Complet, documentation et ajout de commentaires

--- VinlandSol/VinlandMain/IHM/Vinland.xaml.cs
+++ VinlandSol/VinlandMain/IHM/Vinland.xaml.cs
@@ -4,18 +4,28 @@ using System.Windows.Media.Imaging;
 using VinlandMain.IHM;
 using VinlandSol.BDD;
 using VinlandSol.IHM;
-using VinlandSol.Métier;
 
 namespace VinlandSol
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// <author>Aaron</author>
     public partial class MainWindow : Window
     {
+        #region Attributs
+
         private FakeDAO fakeDAO = FakeDAO.Instance;
         private bool isPasswordVisible = false;
-        private int savedCaretIndex = 0;
+
+        #endregion
+
+        #region Constructeur
+
+        /// <summary>
+        /// Initialise la fenêtre
+        /// </summary>
+        /// <author>Aaron</author>
         public MainWindow()
         {
             InitializeComponent();
@@ -23,18 +33,67 @@ namespace VinlandSol
             UpdatePasswordVisibility();
         }
 
+        #endregion
+
+        #region Visibilité mot de passe
+
+        /// <summary>
+        /// Cache ou Révèle le mot de passe en fonction
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <author>Aaron</author>
+        private void MdpVisibilityChanged(object sender, RoutedEventArgs e)
+        {
+            isPasswordVisible = !isPasswordVisible; // On inverse
+            UpdatePasswordVisibility();
+        }
+
+        /// <summary>
+        /// Met à jour la visibilité du mot de passe en fonction de isPasswordVisible
+        /// </summary>
+        /// <author>Alexis + Aaron</author>
+        private void UpdatePasswordVisibility()
+        {
+            if (isPasswordVisible) // Si le mot de passe est visible
+            {
+                TBVisibleMdp.Text = TBMdp.Password; // La textBox avec le texte visible prends la valeur de celle avec le texte caché
+                // On inverse la visibilité des textBox
+                TBMdp.Visibility = Visibility.Collapsed;
+                TBVisibleMdp.Visibility = Visibility.Visible;
+
+                Loeil.Source = new BitmapImage(new Uri("Media/Icones/Oeilbarre.png", UriKind.RelativeOrAbsolute)); // Le bouton change d'image
+            }
+            else
+            {
+                TBMdp.Password = TBVisibleMdp.Text; // La textBox avec le texte caché prends la valeur de celle avec le texte visible 
+                // On inverse la visibilité des textBox
+                TBVisibleMdp.Visibility = Visibility.Collapsed;
+                TBMdp.Visibility = Visibility.Visible;
+
+                Loeil.Source = new BitmapImage(new Uri("Media/Icones/Oeil.png", UriKind.RelativeOrAbsolute)); // Le bouton change d'image
+            }
+        }
+
+        #endregion
+
+        #region Transitions de page
+
         /// <summary>
         /// Ouvre la fenêtre CreationCompte et ferme la fenêtre actuelle
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
+        /// <Author>Aaron</Author>
         private void OuvrirCreationCompte_Click(object sender, RoutedEventArgs e)
         {
-            CreationCompte pagecreation = new CreationCompte();
+            CreationCompte pagecreation = new CreationCompte(); // On créé la nouvelle page
+            // La page s'ouvrira à la même position que la fenêtre actuelle
             pagecreation.Left = this.Left;
             pagecreation.Top = this.Top;
-            pagecreation.Show();
-            Vinland.Close();
+
+            pagecreation.Show(); // On ouvre la nouvelle page
+            Vinland.Close(); // La fenêtre actuelle est fermée
         }
 
         /// <summary>
@@ -42,68 +101,31 @@ namespace VinlandSol
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
+        /// <Author>Aaron</Author>
         private void OuvrirCampagnes_Click(object sender, RoutedEventArgs e)
         {
-            string username = TBNomUtilisateur.Text;
+            string username = TBNomUtilisateur.Text; // On récupère le texte que l'utilisateur a rentré dans le champ Nom d'utilisateur
             string password;
-            if (!isPasswordVisible) { password = TBMdp.Password; } else { password = TBVisibleMdp.Text; }
+            if (!isPasswordVisible) { password = TBMdp.Password; } else { password = TBVisibleMdp.Text; } // On récupère le texte que l'utilisateur a rentré dans le champ Mot de passe 
 
-            (var userId, var userRole) = fakeDAO.VerifyUserAccount(username, password);
+            (var userId, var userRole) = fakeDAO.VerifyUserAccount(username, password); // On vérifie les informations
 
-            if (userRole == "Joueur")
+            if (userRole == "Joueur" || userRole == "MJ") // Si les informations sont correctes
             {
-                Campagnes pagecreation = new Campagnes(userId,userRole);
+                Campagnes pagecreation = new Campagnes(userId, userRole);
+                // La page s'ouvrira à la même position que la fenêtre actuelle
                 pagecreation.Left = this.Left;
                 pagecreation.Top = this.Top;
+
                 pagecreation.Show();
                 Vinland.Close();
             }
-            if (userRole == "MJ")
+            if (userRole == "Non trouvé") // Les informations ne sont pas correctes
             {
-                Campagnes pagecreation = new Campagnes(userId,userRole);
-                pagecreation.Left = this.Left;
-                pagecreation.Top = this.Top;
-                pagecreation.Show();
-                Vinland.Close();
-            }
-            if (userRole == "Non trouvé")
-            {
-                CustomMessageBox messagebox = new CustomMessageBox("Utilisateur non trouvé ou un des champs est incorrect");
+                CustomMessageBox messagebox = new CustomMessageBox("Utilisateur non trouvé ou un des champs est incorrect"); // On en informe l'utilisateur
                 messagebox.ShowDialog();
             }
         }
-
-        /// <summary>
-        /// Cache ou Révèle le mot de passe en fonction
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void MdpVisibilityChanged(object sender, RoutedEventArgs e)
-        {
-            isPasswordVisible = !isPasswordVisible; // On inverse
-
-            UpdatePasswordVisibility();
-        }
-
-        /// <summary>
-        /// Met à jour la visibilité du mot de passe en fonction de isPasswordVisible
-        /// </summary>
-        private void UpdatePasswordVisibility()
-        {
-            if (isPasswordVisible)
-            {
-                TBVisibleMdp.Text = TBMdp.Password;
-                TBMdp.Visibility = Visibility.Collapsed;
-                TBVisibleMdp.Visibility = Visibility.Visible;
-                Loeil.Source = new BitmapImage(new Uri("Media/Icones/Oeilbarre.png", UriKind.RelativeOrAbsolute));
-            }
-            else
-            {
-                TBMdp.Password = TBVisibleMdp.Text;
-                TBVisibleMdp.Visibility = Visibility.Collapsed;
-                TBMdp.Visibility = Visibility.Visible;
-                Loeil.Source = new BitmapImage(new Uri("Media/Icones/Oeil.png", UriKind.RelativeOrAbsolute));
-            }
-        }
+        #endregion
     }
 }
